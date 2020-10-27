d4725beb85113ba57850d680eaad6fa4149429a5
dai <vudinhdai03092001@gmail.com>
2020-10-27 19:26:00 +0700

    p

---
+++ doan1/doan1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doan1
+{
+    class nhanvien
+    {
+        private string manv, tennv;
+        private DateTime ngayvao;
+        private string loainv;
+        private DateTime ngaysinh;
+        private int gioitinh;
+        public nhanvien()
+        {
+            this.manv = "";
+            this.tennv = "";
+            this.ngaysinh =dddd
+            this.loainv = "";
+            this.gioitinh = 0;
+               
+
+                
+        }
+      
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
