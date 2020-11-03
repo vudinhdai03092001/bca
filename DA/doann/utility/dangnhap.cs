7bd0826e9958480e25ebacf42014658fac33084c
dai <vudinhdai03092001@gmail.com>
2020-11-03 21:24:59 +0700

    thêm

---
+++ DA/doann/utility/dangnhap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace doann.utility
+{
+
+    public class DangNhap
+    {
+        private string User;
+        private string Password;
+        public DangNhap()
+        { }
+        public DangNhap(string user, string pass)
+        {
+            this.User = user;
+            this.Password = pass;
+        }
+        public string user
+        {
+            get
+            {
+                return User;
+            }
+            set
+            {
+                if (value != "")
+                    User = value;
+            }
+        }
+        public string pass
+        {
+            get
+            {
+                return Password;
+            }
+            set
+            {
+                if (value != "")
+                    Password = value;
+            }
+        }
+    }
+}
