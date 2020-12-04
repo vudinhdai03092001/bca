8f7a7329ec68e5a25239f6054bee42a3d18035b7
dai <vudinhdai03092001@gmail.com>
2020-12-04 09:56:02 +0700

    thêm

--- DA/doann/businesslayer/Interface/InhanvienBLL.cs
+++ DA/doann/businesslayer/Interface/InhanvienBLL.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using doann.Entities;
 
 namespace doann.businesslayer.Interface
 {
-    interface InhanvienBLL
+   public interface InhanvienBLL
     {
+        List<nhanvien> GetAllnhanvien();
+        void themnhanvien(nhanvien nv);
+        void xoanhanvien(nhanvien manv);
+        void suanhanvien(nhanvien nv);
+        List<NCC> timnhanvien(nhanvien nv);
     }
 }
--- DA/doann/businesslayer/Interface/ItinhluongBLL.cs
+++ DA/doann/businesslayer/Interface/ItinhluongBLL.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using doann.Entities;
 
 namespace doann.businesslayer.Interface
 {
     interface ItinhluongBLL
     {
+        List<tinhluong> GetAlltinhluong();
+        void themtinhluong(tinhluong tl);
+        void xoatinhluong(tinhluong tl);
+        void suatinhluong(tinhluong tl);
+        List<tinhluong> timtinhluong(tinhluong tl);
     }
 }
